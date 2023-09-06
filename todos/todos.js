5a2f66f119e368214d04258f237dddf2d13f13e1
reachbheru <reachbheru@gmail.com>
2023-09-06 21:20:02 +0530

    js file

--- todos/todos.js
+++ todos/todos.js
@@ -5,34 +5,75 @@ let count = 0;
 
 function addTask() {
   let taskName = document.getElementById("inputField").value;
+  if (taskName.trim() !== "") {
+    
+    taskList.push(taskName);
+    let list = document.getElementById("list");
+    list.innerHTML = "";
+    taskList.forEach((element) => {
+      let li = document.createElement("li");
+      li.innerText = element;
+      list.appendChild(li);
+      createCheckbox(list);
+      addRemoveButton(li, element);
 
-  taskList.push(taskName);
+    });
 
-  let list = document.getElementById("list");
-  list.innerHTML = "";
-  taskList.forEach((element) => {
-    let li = document.createElement("li");
-    li.innerText = element;
-    list.appendChild(li);
-    createCheckbox(list);
-  });
+    document.getElementById("inputField").value = "";
+  }
 }
 
 // creating checkbox
 
 function createCheckbox(list) {
+
   const div = document.createElement("div");
   div.className = "checkboxDiv";
   div.id = `checkbox${count}`;
-  
-  div.style.display = "flex";
-  div.style.flexDirection = "column";
-
   const checkbox = document.createElement("input");
   checkbox.type = `checkbox`;
   checkbox.id = `checkboxInput${count}`;
   div.appendChild(checkbox);
-  div.appendChild(document.createElement('br'));
+  div.appendChild(document.createElement("br"));
   list.appendChild(div);
   count++;
+
+}
+
+// addRemoveButton
+
+function addRemoveButton(li, taskName) {
+
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "remove";
+  removeButton.addEventListener("click", () => {
+    removeTask(taskName);
+  });
+
+  li.appendChild(removeButton);
+
+}
+
+function removeTask(taskName) {
+
+  console.log(taskName);
+  console.log(taskList);
+  const list = document.getElementById("list");
+  const index = taskList.indexOf(taskName);
+ 
+  if (index > -1) {
+    taskList.splice(index, 1);
+  }
+
+  list.innerHTML = "";
+
+  taskList.forEach((element) => {
+
+    const li = document.createElement("li");
+    li.innerText = element;
+    list.appendChild(li);
+    addRemoveButton(li, element);
+    createCheckbox(list);
+
+  });
 }
