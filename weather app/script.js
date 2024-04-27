4fa97890a9c4036e85cb1c6cb66a0f9fe499f223
reachbheru <reachbheru@gmail.com>
2024-04-27 17:56:26 +0530

    weather icon update

--- weather app/script.js
+++ weather app/script.js
@@ -1,34 +1,18 @@
 addEventListener("DOMContentLoaded", () => {
-
-  document.querySelector(".search button").addEventListener('click',()=> {
+  document.querySelector(".search button").addEventListener("click", () => {
     let Temp = null;
     let humidity = null;
     let windSpeed = null;
     let name = null;
 
-    // function weatherCondition(condition){
-    //   if (condition === "clear") {
-    //     image.src = "images/clear.png";
-    //   }
-    //   else if(condition === "clouds"){
-    //     image.src = "images/clouds.png"
-    //   }
-    //   else if(condition === "drizzle") {
-    //     image.src = "image/drizzle.png"
-    //   }
-    //   else if(condition === "humidity"){
-    //     image.src = "image/humidity.png"
-    //   }
-    // }
-  
     const city = document.querySelector(".search input").value;
     const temperature = document.querySelector(".temp");
     const locationOutput = document.querySelector(".location");
     const humidityOutput = document.querySelector(".humidityData");
     const windOutput = document.querySelector(".windData");
-    const image = document.querySelector(".image");
-  
-     fetch(
+    const icon = document.querySelector(".image");
+
+    fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2c2d029a529fac94718a7119161f6025&units=metric`
     )
       .then((response) => response.json())
@@ -39,16 +23,37 @@ addEventListener("DOMContentLoaded", () => {
         windSpeed = data.wind.speed;
         name = data.name;
         locationOutput.innerHTML = name;
-        temperature.innerHTML = Math.round(`${Temp}`)+"°C";
+        temperature.innerHTML = Math.round(`${Temp}`) + "°C";
         windOutput.innerHTML = `${windSpeed} Km/h`;
         humidityOutput.innerHTML = `${humidity} %`;
-        // weatherCondition(data.weather.main);
+        if (data.weather[0].main.toLowerCase() == "rain") {
+          icon.outerHTML = '<div class="image"><img src="images/rain.png"></div>';
+          console.log("rain");
+        }
+        if (data.weather[0].main.toLowerCase() == "clouds") {
+          icon.outerHTML = '<div class="image"><img src="images/clouds.png"></div>';
+          console.log("clouds");
+        }
+        if (data.weather[0].main.toLowerCase() == "clear") {
+          icon.outerHTML = '<div class="image"><img src="images/clear.png"></div>';
+          console.log("clear");
+        }
+        if (data.weather[0].main.toLowerCase() == "drizzle") {
+          icon.outerHTML = '<div class="image"><img src="images/drizzle.png"></div>';
+          console.log("drizzle");
+        }
+        if (data.weather[0].main.toLowerCase() == "mist") {
+          icon.outerHTML = '<div class="image"><img src="images/mist.png"></div>';
+          console.log("mist");
+        }
+        if (data.weather[0].main.toLowerCase() == "snow") {
+          icon.outerHTML = '<div class="image"><img src="images/snow.png"></div>';
+          console.log("snow");
+        }
       })
       .catch((error) => {
         console.error("Error:", error);
-        alert("error 404")
+        alert(data.message);
       });
-  })
-
+  });
 });
-
